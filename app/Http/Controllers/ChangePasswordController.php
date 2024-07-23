83275d675b4995becb566df09b4a6e156ffd7c9d
Vincen <vincenhayate123@gmail.com>
2024-07-23 11:27:19 +0700

    Change password works

--- app/Http/Controllers/ChangePasswordController.php
+++ app/Http/Controllers/ChangePasswordController.php
@@ -2,12 +2,71 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class ChangePasswordController extends Controller
 {
     public function index()
     {
         return view('profile.change-password.index');
+    }
+
+    public function changePassword(Request $request)
+    {
+        $validated = $request->validate([
+            'update_password' => 'required|min:6',
+            'confirm_password' => 'required',
+        ]);
+
+        try {
+            DB::beginTransaction();
+
+            if (!Hash::check($validated['confirm_password'], auth()->user()->getAuthPassword())) {
+                DB::rollBack();
+                return back()->withErrors([
+                        'update_password' => ' ',
+                        'confirm_password' => ' ',
+                    ])
+                    ->with([
+                        'status' => 'error',
+                        'message' => 'The password confirmation does not match.'
+                    ]);
+            }
+
+            if (Hash::check($validated['update_password'], auth()->user()->getAuthPassword())) {
+                DB::rollBack();
+                return back()->withErrors([
+                        'update_password' => ' ',
+                        'confirm_password' => ' ',
+                    ])
+                    ->with([
+                        'status' => 'error',
+                        'message' => 'New password cannot be same as current password.'
+                    ]);
+            }
+
+            User::where('user_id', auth()->user()->user_id)
+                ->update([
+                    'password' => Hash::make($validated['update_password']),
+                ]);
+
+            DB::commit();
+            return redirect()
+                ->intended(route('profile.edit-profile-page'))
+                ->with([
+                    'status' => 'success',
+                    'message' => 'Password has been changed successfully.',
+                ]);
+
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return back()->with([
+                'status' => 'error',
+                'message' => 'Invalid operation.'
+            ]);
+        }
     }
 }
--- resources/views/profile/change-password/index.blade.php
+++ resources/views/profile/change-password/index.blade.php
@@ -2,16 +2,45 @@
 @section('title', 'Profile - Change Password')
 
 @section('content')
+
+
     <password-section>
-        <form action="" class="flex flex-col py-10 gap-4">
+        <form action="{{ route('profile.change-password') }}" method="POST" class="flex flex-col pt-10 gap-4">
+            @csrf
+            @method('PUT')
             <password-input class="grid gap-2">
-                <label for="update-password" class="font-mediu">Password</label>
-                <input type="password" name="update-password" id="update-password" class="px-2 py-1 border-2 border-gray-200 rounded-md focus:outline-blue-500 text-gray-500">
-                <p class="text-gray-500">This will be your new account password.</p>
+                <label for="update-password" class="font-medium">Password</label>
+                <input type="password" name="update_password" id="update-password" class="px-2 py-1 border-2 border-gray-200 rounded-md focus:outline-blue-500 text-gray-500
+                    @error('update_password')
+                        border-red-500
+                    @enderror
+                ">
+                @error('update_password')
+                    <p class="text-red-500">{{ $message }}</p>
+                @enderror
             </password-input>
+            <confirm-password class="grid gap-2">
+                <password-input class="grid gap-2">
+                    <label for="confirm-password" class="font-medium">Confirm Password</label>
+                    <input type="password" name="confirm_password" id="confirm-password" class="px-2 py-1 border-2 border-gray-200 rounded-md focus:outline-blue-500 text-gray-500
+                        @error('confirm_password')
+                            border-red-500
+                        @enderror
+                    ">
+                    @error('confirm_password')
+                        <p class="text-red-500">{{ $message }}</p>
+                    @enderror
+                </password-input>
+            </confirm-password>
             <div>
                 <button class="px-5 py-1 text-white text-lg rounded-md bg-blue-600 hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 transition-colors">Update</button>
             </div>
         </form>
     </password-section>
+
+    <script type="module">
+        @if (Session::has('status'))
+            toastr.{{ Session::get('status') }}('{{ Session::get('message') }}');
+        @endif
+    </script>
 @endsection
