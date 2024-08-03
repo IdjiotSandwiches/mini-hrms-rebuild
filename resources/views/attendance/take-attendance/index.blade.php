3297f82263b4d0c62a5ff9af013b390637fa1765
Vin <124791055+IdjiotSandwiches@users.noreply.github.com>
2024-08-03 19:33:18 +0700

    JS Fix (#30)

--- resources/views/attendance/take-attendance/index.blade.php
+++ resources/views/attendance/take-attendance/index.blade.php
@@ -42,27 +42,5 @@
             </form>
         @endif
     </attendance-section>
-    <script type="module">
-        @if (Session::has('status'))
-            toastr.{{ Session::get('status') }}('{{ Session::get('message') }}')
-        @endif
-
-        function getCurrentTime() {
-            let now = new Date();
-            let currentDay = `${now.toLocaleString('default', {weekday: 'long'})}, ${now.getDate()} ${now.toLocaleString('default', {month: 'long'})} ${now.getFullYear()}`;
-            let currentHours = now.getHours().toString().padStart(2, '0');
-            let currentMinutes = now.getMinutes().toString().padStart(2, '0');
-            let currentSeconds = now.getSeconds().toString().padStart(2, '0');
-            $('#current-day').text(currentDay);
-            $('#current-hours').text(currentHours);
-            $('#current-minutes').text(currentMinutes);
-            $('#current-seconds').text(currentSeconds);
-            setTimeout(getCurrentTime, 1000);
-        }
-
-        $(document).ready(function() {
-            getCurrentTime();
-        });
-    </script>
 @endsection
 
