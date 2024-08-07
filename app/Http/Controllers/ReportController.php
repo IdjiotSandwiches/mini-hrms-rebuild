7596fce6410f9848f5dc1db7c6d9a45b5120347d
Vincen <vincenhayate123@gmail.com>
2024-08-07 20:23:48 +0700

    Change Report request type to ReportRequest

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\ReportRequest;
 use App\Services\Attendance\ReportService;
 
 class ReportController extends Controller
@@ -29,7 +29,7 @@ class ReportController extends Controller
         ]);
     }
 
-    public function rangeReport(Request $request)
+    public function rangeReport(ReportRequest $request)
     {
         if (!$request->ajax()) abort(404);
 
