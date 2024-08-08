b0a47fc61797aae07fb78986539e46748d1ca6d1
Vincen <vincenhayate123@gmail.com>
2024-08-08 14:01:00 +0700

    Change datepicker responsive

--- resources/views/attendance/report/index.blade.php
+++ resources/views/attendance/report/index.blade.php
@@ -26,7 +26,7 @@
             ">
                 <div class="
                     relative w-full
-                    sm:w-fit
+                    sm:w-auto
                 ">
                     <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
                         <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
@@ -37,7 +37,7 @@
                 </div>
                 <div class="
                     relative w-full
-                    sm:w-fit
+                    sm:w-auto
                 ">
                     <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
                         <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
@@ -48,7 +48,7 @@
                 </div>
                 <button id="view-report" class="
                     flex w-full justify-center items-center gap-2 py-1.5 px-4 text-white text-lg rounded-md bg-blue-600 hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 transition-colors
-                    sm:w-fit
+                    sm:w-auto
                 ">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" class="fill-white h-5 w-5">
                         <path d="M 21 3 C 11.601563 3 4 10.601563 4 20 C 4 29.398438 11.601563 37 21 37 C 24.355469 37 27.460938 36.015625 30.09375 34.34375 L 42.375 46.625 L 46.625 42.375 L 34.5 30.28125 C 36.679688 27.421875 38 23.878906 38 20 C 38 10.601563 30.398438 3 21 3 Z M 21 7 C 28.199219 7 34 12.800781 34 20 C 34 27.199219 28.199219 33 21 33 C 13.800781 33 8 27.199219 8 20 C 8 12.800781 13.800781 7 21 7 Z"></path>
