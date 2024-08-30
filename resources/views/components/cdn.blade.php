f532abd8c0287b35c1f7905501a7fbfe946c70ba
Vincen <vincenhayate123@gmail.com>
2024-08-30 20:12:15 +0700

    Add apexchartjs cdn

--- resources/views/components/cdn.blade.php
+++ resources/views/components/cdn.blade.php
@@ -2,6 +2,7 @@
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script src="https://cdn.jsdelivr.net/npm/dayjs@1/dayjs.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/dayjs/1.11.7/plugin/customParseFormat.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
 
 <script>
     dayjs().format()
