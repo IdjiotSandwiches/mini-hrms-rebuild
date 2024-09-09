4766ca2a3caf5efaab4f9433b5c340b99f2b3aa1
Vincen <vincenhayate123@gmail.com>
2024-09-09 13:43:49 +0700

    Add search function

--- app/Http/Controllers/ManagementController.php
+++ app/Http/Controllers/ManagementController.php
@@ -9,6 +9,13 @@ class ManagementController extends Controller
 {
     public function index()
     {
+        // return all users data
         return view('admin.management.index');
+    }
+
+    // search using ajax request
+    public function search(Request $request)
+    {
+        if (!$request->ajax()) return 404;
     }
 }
