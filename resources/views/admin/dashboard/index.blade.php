98fafa97885403530a6657de55f2105d0b8a0ad3
Vincen <vincenhayate123@gmail.com>
2024-09-09 11:37:42 +0700

    Add tooltip in weekly chart

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -102,7 +102,7 @@
                         </svg>
                         <div data-popover id="weekly-attendance-chart-info" role="tooltip" class="absolute z-10 invisible inline-block text-sm text-gray-500 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 w-72 dark:bg-gray-800 dark:border-gray-600 dark:text-gray-400">
                             <div class="p-3 space-y-2">
-
+                                Weekly chart of users performance.
                             </div>
                         </div>
                     </div>
