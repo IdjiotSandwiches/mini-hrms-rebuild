d67616c7988ae833dd2a258ef243b78cd1cf2855
Vincen <vincenhayate123@gmail.com>
2024-09-15 20:29:18 +0700

    Change table structure

--- resources/views/admin/management/index.blade.php
+++ resources/views/admin/management/index.blade.php
@@ -21,10 +21,7 @@
                     <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
                             <th scope="col" class="px-6 py-3">
-                                First Name
-                            </th>
-                            <th scope="col" class="px-6 py-3">
-                                Last Name
+                                Fullname
                             </th>
                             <th scope="col" class="px-6 py-3">
                                 Username
@@ -41,11 +38,8 @@
                         @foreach ($users as $user)
                             <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                                 <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                    {{ $user->firstName }}
+                                    {{ "$user->firstName $user->lastName" }}
                                 </th>
-                                <td class="px-6 py-4">
-                                    {{ $user->lastName }}
-                                </td>
                                 <td class="px-6 py-4">
                                     {{ $user->username }}
                                 </td>
@@ -98,8 +92,7 @@
 
                             let row = `
                                 <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">${user.firstName}</th>
-                                    <td class="px-6 py-4">${user.lastName}</td>
+                                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">${user.firstName} ${user.lastName}</th>
                                     <td class="px-6 py-4">${user.username}</td>
                                     <td class="px-6 py-4">${user.email}</td>
                                     <td class="px-6 py-4 text-right">
