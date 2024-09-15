c2fdcbd867b07653c0dcf0907c40ffe8d8a1331f
Vin <124791055+IdjiotSandwiches@users.noreply.github.com>
2024-09-15 22:11:22 +0700

    Admin dashboard (#56)
    * BackMerge from admin to admin-dashboard
    
    * Refactor user id hotfix (#49) (#50)
    
    * Change user_id to id for User Model related
    
    * Change guarded item in Model
    
    ---------
    
    Co-authored-by: Vincen <vincenhayate123@gmail.com>
    
    * Route hotfix (#53)
    
    * Define route & route prefix
    
    * Add dashboard route
    
    * Add management route
    
    ---------
    
    Co-authored-by: Vincen <vincenhayate123@gmail.com>
    
    ---------
    
    Co-authored-by: Vincen <vincenhayate123@gmail.com>
    
    * Minor adjustment
    
    ---------
    
    Co-authored-by: Vincen <vincenhayate123@gmail.com>

--- routes/web_admin.php
+++ routes/web_admin.php
@@ -1,7 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ManagementController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
