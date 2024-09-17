53c95612e1c64833355555a5e0fa62963eb0db43
Vin <124791055+IdjiotSandwiches@users.noreply.github.com>
2024-09-17 11:09:08 +0700

    Minor fix dashboard (#60)
    * Add datatype to function & minor fix
    
    * Update AttendanceInterface, add datatype, & create DashboardInterface
    
    * Add documentator, add return datatype, and update AttendanceInterface
    
    ---------
    
    Co-authored-by: Vincen <vincenhayate123@gmail.com>

--- app/Interfaces/AttendanceInterface.php
+++ app/Interfaces/AttendanceInterface.php
@@ -4,9 +4,11 @@ namespace App\Interfaces;
 
 interface AttendanceInterface
 {
+    public const USER_ID_COLUMN = 'user_id';
     public const CHECK_IN_COLUMN = 'check_in';
     public const CHECK_OUT_COLUMN = 'check_out';
     public const LATE_COLUMN = 'late';
     public const EARLY_COLUMN = 'early';
     public const ABSENCE_COLUMN = 'absence';
+    public const DATE_COLUMN = 'date';
 }
