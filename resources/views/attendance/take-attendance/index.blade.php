a5a5331028d8e64de3729f0156f1b5d0787d8792
Vincen <vincenhayate123@gmail.com>
2024-09-20 11:50:29 +0700

    Minor fix

--- resources/views/attendance/take-attendance/index.blade.php
+++ resources/views/attendance/take-attendance/index.blade.php
@@ -3,28 +3,11 @@
 
 @section('content')
     <attendance-section class="py-10 flex flex-col gap-6 justify-center items-center select-none">
-        <p id="current-day" class="
-            md:text-4xl
-            lg:text-5xl
-            text-3xl text-center font-semibold
-        "></p>
-        <div class="
-            sm:grid sm:grid-cols-2
-            lg:grid-cols-3
-            flex flex-col gap-2
-        ">
-            <div class="
-                flex justify-center items-center w-48 h-32 bg-gray-300 rounded-md font-semibold text-8xl shadow-md
-                dark:bg-gray-600
-            " id="current-hours"></div>
-            <div class="
-                flex justify-center items-center w-48 h-32 bg-gray-300 rounded-md font-semibold text-8xl shadow-md
-                dark:bg-gray-600
-            " id="current-minutes"></div>
-            <div class="
-                hidden lg:flex justify-center items-center w-48 h-32 bg-gray-300 rounded-md font-semibold text-8xl shadow-md
-                dark:bg-gray-600
-            " id="current-seconds"></div>
+        <p id="current-day" class="md:text-4xl lg:text-5xl text-3xl text-center font-semibold"></p>
+        <div class="sm:grid sm:grid-cols-2 lg:grid-cols-3 flex flex-col gap-2">
+            <div class="flex justify-center items-center w-48 h-32 bg-gray-300 rounded-md font-semibold text-8xl shadow-md dark:bg-gray-600" id="current-hours"></div>
+            <div class="flex justify-center items-center w-48 h-32 bg-gray-300 rounded-md font-semibold text-8xl shadow-md dark:bg-gray-600" id="current-minutes"></div>
+            <div class="hidden lg:flex justify-center items-center w-48 h-32 bg-gray-300 rounded-md font-semibold text-8xl shadow-md dark:bg-gray-600" id="current-seconds"></div>
         </div>
         <form action="{{ route('attendance.take-attendance') }}" method="POST">
             @csrf
