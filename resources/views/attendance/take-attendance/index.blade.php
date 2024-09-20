c1829c6159b5a94369da83e1d95c26308b9a5146
Vincen <vincenhayate123@gmail.com>
2024-09-20 18:09:25 +0700

    Loading overlay & disable button css

--- resources/views/attendance/take-attendance/index.blade.php
+++ resources/views/attendance/take-attendance/index.blade.php
@@ -2,6 +2,7 @@
 @section('title', 'Attendance - Take Attendance')
 
 @section('content')
+    @include('components.loading-overlay')
     <attendance-section class="py-10 flex flex-col gap-6 justify-center items-center select-none">
         <p id="current-day" class="md:text-4xl lg:text-5xl text-3xl text-center font-semibold"></p>
         <div class="sm:grid sm:grid-cols-2 lg:grid-cols-3 flex flex-col gap-2">
@@ -11,7 +12,7 @@
         </div>
         <form action="{{ route('attendance.take-attendance') }}" method="POST">
             @csrf
-            <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-md w-full sm:w-auto px-20 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            <button type="submit" class="disabled:bg-blue-400 disabled:dark:bg-blue-500 disabled:cursor-not-allowed text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-md w-full sm:w-auto px-20 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                 @if($isCheckedIn)
                     Check Out
                 @else
