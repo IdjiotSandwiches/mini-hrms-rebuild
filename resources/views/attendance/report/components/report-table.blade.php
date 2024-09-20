2675e708b104f6e306480204d05e0bcef19262d2
Vincen <vincenhayate123@gmail.com>
2024-09-20 20:03:20 +0700

    Make custom report to use report-table component

--- resources/views/attendance/report/components/report-table.blade.php
+++ resources/views/attendance/report/components/report-table.blade.php
@@ -1,6 +1,8 @@
-@props(['attendances'])
-<div class="relative overflow-x-auto sm:rounded-lg">
+@props(['attendances' => collect(), 'id' => ''])
+
+<div class="relative overflow-x-auto sm:rounded-lg" id="{{ $id }}">
     <table class="w-full text-sm text-center rtl:text-right text-gray-500 dark:text-gray-400">
+        @include('components.loading-overlay')
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
                 <th scope="col" class="px-6 py-3">No.</th>
--- resources/views/attendance/report/index.blade.php
+++ resources/views/attendance/report/index.blade.php
@@ -24,37 +24,12 @@
                     View
                 </button>
             </div>
-            <div class="relative overflow-x-auto sm:rounded-lg" id="custom-report">
-                <table class="w-full text-sm text-center rtl:text-right text-gray-500 dark:text-gray-400">
-                    @include('components.loading-overlay')
-                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                        <tr>
-                            <th scope="col" class="px-6 py-3">No.</th>
-                            <th scope="col" class="px-6 py-3">Date</th>
-                            <th scope="col" class="px-6 py-3">Check In Time</th>
-                            <th scope="col" class="px-6 py-3">Check Out Time</th>
-                            <th scope="col" class="px-6 py-3">Early</th>
-                            <th scope="col" class="px-6 py-3">Late</th>
-                            <th scope="col" class="px-6 py-3">Absence</th>
-                            <th scope="col" class="px-6 py-3">Work Duration</th>
-                        </tr>
-                    </thead>
-                        <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                            <td colspan="8" class="px-6 py-3 text-center">
-                                You do not have work attendance
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
+            @include('attendance.report.components.report-table', with(['id' => 'custom-report']))
         </custom-report>
         <weekly-table class="gap-4 flex flex-col">
             <div>
                 <h1 class="text-lg font-medium">Weekly Report</h1>
-                <p class="
-                    text-gray-500
-                    dark:text-gray-300
-                ">This is your work report for the last 7 days.</p>
+                <p class="text-gray-500 dark:text-gray-300">This is your work report for the last 7 days.</p>
             </div>
             @include('attendance.report.components.report-table', with(['attendances' => $weeklyAttendances]))
             <p class="font-medium">Total Weekly Work Hours:
@@ -67,19 +42,17 @@
         <monthly-table class="gap-4 flex flex-col">
             <div>
                 <h1 class="text-lg font-medium">Monthly Report</h1>
-                <p class="
-                    text-gray-500
-                    dark:text-gray-300
-                ">This is your work report for the last 30 days.</p>
+                <p class="text-gray-500 dark:text-gray-300">This is your work report for the last 30 days.</p>
             </div>
             @include('attendance.report.components.report-table', with(['attendances' => $monthlyAttendances]))
+            {{ $monthlyAttendances->links('pagination::tailwind') }}
+
             <p class="font-medium">Total Monthly Work Hours:
                 <span @class([
                     'text-red-500' => $monthlyWorkHours < 80,
                     'text-blue-500'
                 ])>{{ $monthlyWorkHours }} Hours</span>
             </p>
-            {{ $monthlyAttendances->links('pagination::tailwind') }}
         </monthly-table>
     </report-section>
 @endsection
@@ -94,6 +67,14 @@
                 data: {
                     start_time: start,
                     end_time: end,
+                },
+                beforeSend: function() {
+                    $('#loading-overlay').show();
+                    $('button').prop('disabled', true);
+                },
+                complete: function() {
+                    $('#loading-overlay').hide();
+                    $('button').prop('disabled', false);
                 },
                 success: function(res) {
                     const table = $('#custom-report').find('tbody');
