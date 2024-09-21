1019df485f741665dfee874158fb1bee8d746452
Vin <124791055+IdjiotSandwiches@users.noreply.github.com>
2024-09-21 23:19:18 +0700

    Fix route navbar (#95)
    * Refactor navbar js, rerouting necessary to new route
    
    * Route changes for every prefix that need 1 route to be /
    
    * Change user home route
    
    * Change logout button to a form
    
    * Add disabled button css
    
    ---------
    
    Co-authored-by: Vincen <vincenhayate123@gmail.com>

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,15 +1,15 @@
 <nav class="bg-white fixed z-10 w-full flex justify-between items-center border-gray-200 border-b-[0.5px] px-10 h-16 select-none md:px-28 dark:bg-[#121212] dark:text-white">
     @auth
-        <a class="text-3xl font-semibold dark:text-white" href="">hrms.</a>
-        <section class="profile flex items-center gap-4">
+        <a class="text-3xl font-semibold dark:text-white" href="/">hrms.</a>
+        <section id="nav-profile" class="flex items-center gap-4">
             <h1 class="hidden sm:block">Welcome, <span class="text-blue-400">{{ auth()->user()->first_name }} {{ auth()->user()->last_name }}</span></h1>
             <img src="{{ asset(auth()->user()->avatar) }}" class="h-10 w-10 rounded-full"/>
         </section>
     @else
-        <a class="text-3xl font-semibold" href="{{ route('landing-page') }}">hrms.</a>
+        <a class="text-3xl font-semibold" href="{{ route('login') }}">hrms.</a>
         <nav-button class="md:flex md:gap-2 hidden">
-            @include('components.navbar-item', with(['name' => 'Login', 'path' => ['login', 'landing-page']]))
-            @include('components.navbar-item', with(['name' => 'Register', 'path' => ['register']]))
+            @include('components.navbar-item', with(['name' => 'Login', 'path' => 'login']))
+            @include('components.navbar-item', with(['name' => 'Register', 'path' => 'register']))
         </nav-button>
     @endauth
 </nav>
@@ -29,28 +29,9 @@
                 @include('components.dropdown-item', with(['name' => 'Attendance', 'path' => 'attendance.take-attendance-page']))
             @endif
         </div>
-        <div class="select-none grid">
-            <a href="{{ route('logout') }}" class="py-2 px-4 hover:bg-gray-100 transition-colors text-sm dark:hover:bg-gray-600">Logout</a>
-        </div>
+        <form action="{{ route('logout') }}" method="GET" class="select-none grid">
+            @csrf
+            <button type="submit" class="py-2 px-4 hover:bg-gray-100 transition-colors text-sm text-left dark:hover:bg-gray-600">Logout</button>
+        </form>
     </nav-item>
 @endauth
-
-@section('extra-js')
-    <script>
-        $(document).ready(function() {
-            $('.profile').click(function(event) {
-                $('.nav-dropdown').slideToggle('hidden');
-                event.stopPropagation();
-            });
-
-            $(document).on('click', function(event) {
-                let trigger = $('.nav-dropdown');
-                let navItem = $(event.target).closest('nav-item');
-
-                if(trigger[0] !== navItem[0]){
-                    trigger.slideUp('fast');
-                }
-            });
-        });
-    </script>
-@endsection
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -24,7 +24,7 @@
                 <p class="text-red-500 text-sm">{{ $message }}</p>
             @enderror
         </password-form>
-        <button type="submit" class="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-md px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</button>
+        <button type="submit" class="disabled:bg-blue-400 disabled:dark:bg-blue-500 disabled:cursor-not-allowed w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-md px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</button>
         <p class="text-center text-sm">
             Don't have an account?
             <a href="{{ route('register') }}" class="font-semibold leading-6 text-blue-600 transition-colors hover:text-blue-500 underline decoration-2 underline-offset-2">Register</a>
