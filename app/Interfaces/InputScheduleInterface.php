d0e81c26fe03be3f9736323b85f0cba407c44a00
Vincen <vincenhayate123@gmail.com>
2024-09-21 17:09:40 +0700

    Create input schedule interface

--- app/Interfaces/InputScheduleInterface.php
+++ app/Interfaces/InputScheduleInterface.php
@@ -1,1 +1,11 @@
+<?php
 
+namespace App\Interfaces;
+
+interface InputScheduleInterface
+{
+    public const DAY_COLUMN = 'day';
+    public const START_TIME_COLUMN = 'start_time';
+    public const END_TIME_COLUMN = 'end_time';
+    public const WORK_TIME_COLUMN = 'work_time';
+}
