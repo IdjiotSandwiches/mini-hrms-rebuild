702f3d91c2a60820c59f87ab20d75757ddda1d1f
Vincen <vincenhayate123@gmail.com>
2024-09-21 17:06:04 +0700

    Create user interface

--- app/Interfaces/UserInterface.php
+++ app/Interfaces/UserInterface.php
@@ -1,1 +1,16 @@
+<?php
 
+namespace App\Interfaces;
+
+interface UserInterface
+{
+    public const ID_COLUMN = 'id';
+    public const UUID_COLUMN = 'uuid';
+    public const FIRST_NAME_COLUMN = 'first_name';
+    public const LAST_NAME_COLUMN = 'last_name';
+    public const USERNAME_COLUMN = 'username';
+    public const EMAIL_COLUMN = 'email';
+    public const PASSWORD_COLUMN = 'password';
+    public const AVATAR_COLUMN = 'avatar';
+    public const LAST_PASSWORD_CHANGE_COLUMN = 'last_password_change';
+}
