2de73f89bf07a8b85086630059e4f791e0895137
Vincen <vincenhayate123@gmail.com>
2024-09-21 17:44:23 +0700

    Implement interface in login service

--- app/Services/LoginService.php
+++ app/Services/LoginService.php
@@ -2,11 +2,13 @@
 
 namespace App\Services;
 
+use App\Interfaces\UserInterface;
 use App\Models\User;
 use App\Models\Admin;
 use Illuminate\Support\Facades\Hash;
 
-class LoginService extends BaseService
+class LoginService extends BaseService implements
+    UserInterface
 {
     /**
      * @param array
@@ -14,14 +16,14 @@ class LoginService extends BaseService
      */
     public function attemptLogin($validated)
     {
-        $user = User::where('email', $validated['email'])->first() ??
-            Admin::where('email', $validated['email'])->first();
+        $user = User::where(self::EMAIL_COLUMN, $validated[self::EMAIL_COLUMN])->first() ??
+            Admin::where(self::EMAIL_COLUMN, $validated[self::EMAIL_COLUMN])->first();
 
         if (!$user) {
             return $user;
         }
 
-        if (!Hash::check($validated['password'], $user->password)) {
+        if (!Hash::check($validated[self::PASSWORD_COLUMN], $user->password)) {
             return $user;
         }
 
