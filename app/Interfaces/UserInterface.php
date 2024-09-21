16a81216a29ce3da08b8f7666323ae8b921290db
Vincen <vincenhayate123@gmail.com>
2024-09-21 17:09:57 +0700

    Remove id from user interface

--- app/Interfaces/UserInterface.php
+++ app/Interfaces/UserInterface.php
@@ -4,7 +4,6 @@ namespace App\Interfaces;
 
 interface UserInterface
 {
-    public const ID_COLUMN = 'id';
     public const UUID_COLUMN = 'uuid';
     public const FIRST_NAME_COLUMN = 'first_name';
     public const LAST_NAME_COLUMN = 'last_name';
