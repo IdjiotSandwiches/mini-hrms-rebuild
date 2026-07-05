ad8af6cdfed070f013de569d5d6d1357808759b5
IdjiotSandwiches <idjiotsandwiches@gmail.com>
2026-07-06 00:39:52 +0700

    fix: redirect to legacy version

--- resources/js/layouts/app/AppHeaderLayout.vue
+++ resources/js/layouts/app/AppHeaderLayout.vue
@@ -7,7 +7,8 @@ import { Toaster } from '@/components/ui/sonner';
 import type { BreadcrumbItem } from '@/types';
 import { usePage } from '@inertiajs/vue3';
 import { computed } from 'vue';
-import { getAdminHome } from '@/lib/utils';
+import { index as attendanceIndex } from '@/routes/v1/take-attendance';
+import { index as dashboardIndex } from '@/routes/v1/admin/dashboard';
 
 type Props = {
     breadcrumbs?: BreadcrumbItem[];
@@ -30,7 +31,7 @@ const auth = computed(() => page.props.auth);
         </AppContent>
         <Toaster :rich-colors="true" />
 
-        <a class="fixed bottom-5 left-1/2 -translate-x-1/2 -translate-y-1/2" :href="getAdminHome(auth.can.admin).url">
+        <a class="fixed bottom-5 left-1/2 -translate-x-1/2 -translate-y-1/2" :href="auth.can.admin ? dashboardIndex().url : attendanceIndex().url">
             <Button class="bg-blue-600 dark:text-primary hover:bg-blue-600/80 shadow-lg/30">Go to Legacy Version</Button>
         </a>
     </AppShell>
