0eaac7e14a280fa85217edbcfe07e4f1a5f5e65c
IdjiotSandwiches <idjiotsandwiches@gmail.com>
2026-07-06 00:32:23 +0700

    feat: remove settings file routes

--- routes/v2.php
+++ routes/v2.php
@@ -88,4 +88,9 @@ Route::middleware(['auth', 'verified'])
             });
     });
 
-
+Route::get('.well-known/passkey-endpoints', function () {
+    return response()->json([
+        'enroll' => route('v2.settings.security.edit'),
+        'manage' => route('v2.settings.security.edit'),
+    ]);
+})->name('well-known.passkeys');
