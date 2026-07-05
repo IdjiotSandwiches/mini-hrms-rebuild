a4532f110c54ee279e74699d54d9dd6c248a33e4
IdjiotSandwiches <idjiotsandwiches@gmail.com>
2026-07-06 00:32:48 +0700

    feat: clean web routes

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::redirect('/', '/login')->middleware(['guest']);
 Route::redirect('/', '/v2/take-attendance')->middleware(['auth', 'verified']);
 
-require __DIR__.'/settings.php';
 require __DIR__.'/v1.php';
 require __DIR__.'/v2.php';
