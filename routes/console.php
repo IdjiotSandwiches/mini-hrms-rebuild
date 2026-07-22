a9d1f5c2f32efd3d1330dddd591f342de5b8f68e
IdjiotSandwiches <idjiotsandwiches@gmail.com>
2026-07-22 22:29:22 +0700

    feat: modify docker & ci-cd

--- routes/console.php
+++ routes/console.php
@@ -9,4 +9,6 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command(MarkAsAbsence::class)->daily();
+Schedule::command(MarkAsAbsence::class)
+    ->daily()
+    ->onOneServer();
